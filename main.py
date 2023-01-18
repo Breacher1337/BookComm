f85bb956986be9d3feb44312e1562c30678ac1e5
Breacher1337 <elaijahangelo@gmail.com>
2023-01-18 23:02:21 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Response
 from jinja2 import Template
+import requests
+
+GOOGLE_HREF = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
 
 app = FastAPI()
 
@@ -8,3 +11,13 @@ async def root():
     template = Template(open("templates/index.html").read())
     content = template.render()
     return Response(content=content, media_type="text/html")
+
+
+@app.get("/scrape_book/{book_id}")
+async def scrape_book(book_id: int):
+    params = {
+        "q": "9780321127310"
+    }
+    response = requests.get(GOOGLE_HREF, params=params)
+
+    return response.json()
